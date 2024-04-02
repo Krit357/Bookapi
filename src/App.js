e58c9a00243402c69a0113f29e1ff9d16ae3ab7e
Chris357 <Chris@eup.co.th>
2024-04-02 10:54:34 +0700

    add-axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 const App = () => {
   const [books, setBooks] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const createBook = (title) => {
-    // const updateBook = [...book, { id: 123, title: title }];
-    // setBook(updateBook);
-    const updateBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  // DONT DO THIS this will cause infinite loop
+  // fetchBooks();
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updateBooks = [...books, response.data];
     setBooks(updateBooks);
   };
+  const deleteBookById = async (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this book"
+    );
 
-  const deleteBookById = (id) => {
-    const deleteBook = books.filter((book) => {
-      return book.id !== id;
-    });
-    setBooks(deleteBook);
+    if (!confirmDelete) return;
+    try {
+      await axios.delete(`http://localhost:3001/books/${id}`);
+      const deleteBook = books.filter((book) => {
+        return book.id !== id;
+      });
+      setBooks(deleteBook);
+      setShowAlert(true);
+    } catch (err) {
+      console.log("Error deleting Book", err);
+      window.alert("An error occurred while deleting the book.");
+    }
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    console.log(response);
     const updateBook = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
